dd16376b6083b0cf68a9a9fffe87893db950e010
muhammed1412 <muhammed@retrace.ai>
2024-02-18 16:11:15 +0300

    create db

---
+++ Infrastucture/AmazonOnionApi.Persistence/Registration.cs
@@ -0,0 +1,21 @@
+﻿using AmazonOnionApi.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonOnionApi.Persistence
+{
+    public static class Registration
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
