54692454fe3b1a36e91691bf1e0b2053049f4f94
muhammed1412 <muhammed@retrace.ai>
2024-04-06 18:10:51 +0300

    update

--- Presentation/AmazonOnionApi.Api/Controllers/AuthController.cs
+++ Presentation/AmazonOnionApi.Api/Controllers/AuthController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AmazonOnionApi.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
